26eebd88204cd7b543b005cfea52b4195b53c942
juanjoseleongil <53834551+juanjoseleongil@users.noreply.github.com>
2020-05-05 14:01:16 -0400

    Actualizacion, codigo motor

--- Parcial3/UsandoClases/EllipticPoisson.cpp
+++ Parcial3/UsandoClases/EllipticPoisson.cpp
@@ -1,37 +1,41 @@
-//Applitcation of class "solvePDE"
+//Application of class "solvePDE"
 #include <iostream>
 #include <iomanip>
 #include <cmath>
 #include <vector>
 #include "solvePDE.h"
 
-#define myPrint std::setw(5) << std::setprecision(3)
+#define myPrint std::setw(4) << std::setprecision(4)
 
 int main()
 { 
   //object creation
   solvePDE poisson;
   
-  //print initial values
-  std::cout << "Test printing of initial values" << std::endl;
-  std::cout << "i\tj\txi\tyi\tf(xi,yi)" << std::endl;
+  //configure b
+  poisson.setSysb();
+  
+  //configure M
+  poisson.setSysM();
+  
+  //solve System
+  poisson.GaussLinearSolve();
+  
+  //print solution
+  std::cout << "Solution printing" << std::endl;
+  std::cout << "i\tj\txi\tyj\tw(i,j)\tu(i,j)" << std::endl;
   for (unsigned int i = 1; i < poisson.getNx(); i++)
   {
     for (unsigned int j = 1; j < poisson.getNy(); j++)
     {
+      unsigned int l = i - 1 + (poisson.getNx() - 1) * (j - 1);
       std::cout << i << "\t" << j << "\t"
                 << myPrint << poisson.getX(i) << "\t" 
                 << myPrint << poisson.getY(j) << "\t"
+                << myPrint << poisson.getSysb(l) << "\t"
                 << myPrint << poisson.source(i, j) << std::endl;
     }
   }
-  
-  std::cout << "\nSystem matrix setup and printing" << std::endl;
-  //configure M
-  poisson.setSysM();
-  
-  //print M
-  poisson.printSysM();
-  
+
   return(0);
 } //end main
