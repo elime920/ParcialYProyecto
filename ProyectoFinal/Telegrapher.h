62b5e051c0a5f7bab1918ac22a9458918ba01ff4
elime920 <53834626+elime920@users.noreply.github.com>
2020-05-21 00:02:13 -0500

    Definición

---
+++ ProyectoFinal/Telegrapher.h
@@ -0,0 +1,51 @@
+//poisson2D class declaration. Member functions defined in poisson2D.cpp
+#include <vector>
+#include <functional>
+
+#ifndef TELEGRAPHER_H
+#define TELEGRAPHER_H
+
+
+
+class Telegrapher
+{
+ public:
+  Telegrapher(double, double, double, double, double,
+	      double, double, unsigned int, unsigned int,
+	      std::function<double(unsigned int, unsigned int)>,
+	      std::function<double(unsigned int, unsigned int)>);
+
+  //destructor
+  ~Telegrapher();
+
+    //set parameters appearing in the difference equation
+  void setComputationParams();
+
+    //auxiliary function to assign the source and boundary functions
+  double evalOnIndices(unsigned int, unsigned int,
+		       std::function<double(unsigned int, unsigned int)>);
+
+    //source function data type
+  std::function<double(unsigned int, unsigned int)> sFunc;
+    //boundary function data t
+  std::function<double(unsigned int, unsigned int)> bFunc;
+  //source function prototype
+  double source(unsigned int, unsigned int);
+    //boundary function prototype
+  double boundary(unsigned int, unsigned int);
+  void setfirstline(unsigned int);
+  void Bounds(unsigned int, unsigned int);
+  void Inner(unsigned int, unsigned int);
+  double getSln(unsigned int, unsigned int);
+ private:
+  unsigned short int NT, NZ;
+  double T0, T;
+  double Z0, ZF;
+  double lambda, alpha, beta; //difference equation parameters
+  double mu, sigma, nu, k;
+  double hT, hZ;
+  std::vector<std::vector<double>> W;
+
+};
+
+#endif
