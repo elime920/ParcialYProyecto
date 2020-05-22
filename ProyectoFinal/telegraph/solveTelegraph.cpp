506b7abbf9bdb3c563d96f381bc264e3dc385756
juanjoseleongil <53834551+juanjoseleongil@users.noreply.github.com>
2020-05-21 22:01:29 -0400

    solve and print voltage and current

--- ProyectoFinal/telegraph/solveTelegraph.cpp
+++ ProyectoFinal/telegraph/solveTelegraph.cpp
@@ -4,43 +4,50 @@
 #include "telegraph.h"
 #include "examples/example1.h"
 
-#define printIndex std::left << std::setw(3)
-#define printReal std::right << std::setw(10) << std::fixed << std::setprecision(4)
-#define printComp std::setw(10) << std::scientific << std::setprecision(3)
+#define printIndex std::left << std::setw(2)
+#define printReal std::right << std::setw(7) << std::fixed << std::setprecision(4)
+#define printComp std::setw(7) << std::scientific << std::setprecision(1)
 
 int main()
 {
   //object creation: bvp stands for Boundary Value Problem
-  telegraph bvp(T0, TF, Z0, ZF, NT, NZ, k,
-                boundFunc, initDerFunc);
+  telegraph bvp(endpoints, params, bcV, bcI, NT, NZ);
 
-  //solve problem
-  bvp.setw();
+  bvp.setwV(); //solve for voltage
+  bvp.setwI(); //solve for current
     
   //print solution
-  double exSol = 0.0;
+  double vSol = 0.0, iSol = 0.0;
   std::cout << "Solution printing:" << std::endl;
-  std::cout << "i   j       xi         yj         "
-               "w(i,j)     u(i,j)    |u-w|" << std::endl;
+  std::cout << "i  j | ti      zj     | wV(i,j) V(i,j) | |V-wV| |"
+               " wI(i,j) I(i,j) | |I-wI| " << std::endl;
+  
+  for (unsigned int l = 0; l < 74; l++) std::cout << "-"; 
+  std::cout << std::endl;
+  
   for (unsigned int i = 0; i <= NT; i++)
   {
     for (unsigned int j = 0; j <= NZ; j++)
     {
-      exSol = exSln(i, j); //analytic solution
+      vSol = vSln(bvp.getT(i)/bvp.Jt, bvp.getZ(j)/bvp.Jz);
+      iSol = iSln(bvp.getT(i)/bvp.Jt, bvp.getZ(j)/bvp.Jz);
+      
       std::cout << printIndex << i << " " 
-                << printIndex << j << " "
-                << printReal << T(i) << " " 
-                << printReal << Z(j) << " "
-                << printReal << bvp.getSln(i, j) << " "
-                << printReal << exSol << "  | " 
-                << printComp << fabs(exSol - bvp.getSln(i, j))
+                << printIndex << j << "|"
+                << printReal << bvp.getT(i)/bvp.Jt << " " 
+                << printReal << bvp.getZ(j)/bvp.Jz << " |"
+                << printReal << bvp.getwV(i, j) << " "
+                << printReal << vSol << " |" 
+                << printComp << fabs(vSol - bvp.getwV(i, j)) << "||"
+                << printReal << bvp.getwI(i, j) << " "
+                << printReal << iSol << " |" 
+                << printComp << fabs(iSol - bvp.getwI(i, j)) << "|"
                 << std::endl;
     }
   } //end printing
   
   //write to files
-  bvp.saveAsMatrix("outputMat.dat"); //save as matrix data
-  bvp.saveAsColumns("outputCols.dat"); //save as column data
+  //bvp.saveToFile("V", "Columns", "outputCols.dat");
 
   return(0);
 } //end main
